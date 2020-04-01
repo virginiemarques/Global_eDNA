335de1cce76179b8618e469277c93d00f54a8139
root <root@EB10PA4.newcefe.newage.fr>
2020-04-01 02:28:07 +0200

    family_proportions

--- scripts/02_exploration/05_family_proportions.R
+++ scripts/02_exploration/05_family_proportions.R
@@ -9,14 +9,14 @@ load("Rdata/02_clean_all.Rdata")
 
   ## Caribbean total
 caribbean <- df_all_filters %>%
-  filter(region=="Caribbean")
+  filter(region=="Caribbean" & !is.na(new_family_name))
 
-otu_names_family <- caribbean %>%
-  distinct(sequence, new_rank_ncbi, new_family_name, new_scientific_name_ncbi, best_identity_database)
+count_families_caribbean <- data.frame(table(caribbean$new_family_name))
+colnames(count_families_caribbean) <- c("family", "n_motus")
+count_families_caribbean$n_motus_total <- nrow(caribbean)
+count_families_caribbean$prop <- count_families_caribbean$n_motus / count_families_caribbean$n_motus_total
 
-count_families_caribbean <- data.frame(table(otu_names_family$new_family_name))
-
-ggplot(count_families_caribbean, aes(x=reorder(Var1, Freq), y = Freq, fill = Freq)) + 
+ggplot(count_families_caribbean, aes(x=reorder(family, prop), y = prop, fill = prop)) + 
   geom_bar(stat="identity") + 
   theme_bw() +
   labs(x="Family", y="Frequency")+
@@ -34,10 +34,12 @@ count_families_site_caribbean=NULL
 
 for (i in 1:length(site)) {
   s <- site[i]
-  otu_names_family <- caribbean[caribbean$site == site[i],] %>%
-    distinct(sequence, new_rank_ncbi, new_family_name, new_scientific_name_ncbi, best_identity_database)
-  count_families <- data.frame(table(otu_names_family$new_family_name))
-  count_families <- count_families[order(count_families$Freq, decreasing = TRUE),]
+  cari_site <- caribbean[caribbean$site == site[i],]
+  count_families <- data.frame(table(cari_site$new_family_name))
+  colnames(count_families) <- c("family", "n_motus")
+  count_families$n_motus_total <- nrow(cari_site)
+  count_families$prop <- count_families$n_motus / count_families$n_motus_total
+  count_families <- count_families[order(count_families$prop, decreasing = TRUE),]
   count_families$site <- s
   count_families_site_caribbean <- rbind(count_families_site_caribbean, count_families)
 }
@@ -46,10 +48,10 @@ write.csv(count_families_site_caribbean, "outputs/05_family_proportion/family_fr
 
 count_families_site_caribbean <- count_families_site_caribbean %>%
   ungroup() %>%
-  arrange(site, Freq) %>%
+  arrange(site, prop) %>%
   mutate(order = row_number())
 
-ggplot(count_families_site_caribbean, aes(order, Freq, fill = Freq)) + 
+ggplot(count_families_site_caribbean, aes(order, prop, fill = prop)) + 
   geom_bar(stat="identity") +
   facet_wrap(~site, scales ="free_y", as.table = FALSE)+
   theme_bw() +
@@ -57,7 +59,7 @@ ggplot(count_families_site_caribbean, aes(order, Freq, fill = Freq)) +
   theme(axis.text.x=element_text(angle = 0, hjust = 0)) +
   theme(legend.position = "none")+
   coord_flip()+
-  scale_x_continuous(breaks=count_families_site_caribbean$order, labels=count_families_site_caribbean$Var1, expand = c(0,0))
+  scale_x_continuous(breaks=count_families_site_caribbean$order, labels=count_families_site_caribbean$family, expand = c(0,0))
 
 ggsave("outputs/05_family_proportion/family_frequency_site_caribbean.png", width=20, height=16)
 
@@ -66,15 +68,15 @@ ggsave("outputs/05_family_proportion/family_frequency_site_caribbean.png", width
   ## Lengguru total
 
 lengguru <- df_all_filters %>%
-  filter(region=="West_Papua")
+  filter(region=="West_Papua" & !is.na(new_family_name))
 
-otu_names_family <- lengguru %>%
-  distinct(sequence, new_rank_ncbi, new_family_name, new_scientific_name_ncbi, best_identity_database)
+count_families_lengguru <- data.frame(table(lengguru$new_family_name))
+colnames(count_families_lengguru) <- c("family", "n_motus")
+count_families_lengguru$n_motus_total <- nrow(lengguru)
+count_families_lengguru$prop <- count_families_lengguru$n_motus / count_families_lengguru$n_motus_total
 
 
-count_families_lengguru <- data.frame(table(otu_names_family$new_family_name))
-
-ggplot(count_families_lengguru, aes(x=reorder(Var1, Freq), y = Freq, fill = Freq)) + 
+ggplot(count_families_lengguru, aes(x=reorder(family, prop), y = prop, fill = prop)) + 
   geom_bar(stat="identity") + 
   theme_bw() +
   labs(x="Family", y="Frequency")+
@@ -93,10 +95,12 @@ count_families_site_lengguru=NULL
 
 for (i in 1:length(site)) {
   s <- site[i]
-  otu_names_family <- lengguru[lengguru$site == site[i],] %>%
-    distinct(sequence, new_rank_ncbi, new_family_name, new_scientific_name_ncbi, best_identity_database)
-  count_families <- data.frame(table(otu_names_family$new_family_name))
-  count_families <- count_families[order(count_families$Freq, decreasing = TRUE),]
+  leng_site <- lengguru[lengguru$site == site[i],]
+  count_families <- data.frame(table(leng_site$new_family_name))
+  colnames(count_families) <- c("family", "n_motus")
+  count_families$n_motus_total <- nrow(leng_site)
+  count_families$prop <- count_families$n_motus / count_families$n_motus_total
+  count_families <- count_families[order(count_families$prop, decreasing = TRUE),]
   count_families$site <- s
   count_families_site_lengguru <- rbind(count_families_site_lengguru, count_families)
 }
@@ -105,14 +109,14 @@ write.csv(count_families_site_lengguru, "outputs/05_family_proportion/family_fre
 
 count_families_site_lengguru <- count_families_site_lengguru %>%
   ungroup() %>%
-  arrange(site, Freq) %>%
+  arrange(site, prop) %>%
   mutate(order = row_number())
 
   ## plot by 3 sites, because plot too small otherwise
-site_sub <- c("tanjung_boi", "triton_bay")
+site_sub <- c("lobo", "pulau_aiduma", "pulau_aiduma_ext")
 subset1 <- count_families_site_lengguru %>%
   filter(site%in%site_sub)
-ggplot(subset1, aes(order, Freq, fill = Freq)) + 
+ggplot(subset1, aes(order, prop, fill = prop)) + 
   geom_bar(stat="identity") +
   facet_wrap(~site, scales ="free_y")+
   theme_bw() +
@@ -120,10 +124,10 @@ ggplot(subset1, aes(order, Freq, fill = Freq)) +
   theme(axis.text.x=element_text(angle = 0, hjust = 0)) +
   theme(legend.position = "none")+
   coord_flip()+
-  scale_x_continuous(breaks=subset1$order, labels=subset1$Var1, expand = c(0,0))
+  scale_x_continuous(breaks=subset1$order, labels=subset1$family, expand = c(0,0))
 
 
-ggsave("outputs/05_family_proportion/family_frequency_lengguru_site10-11.png", width=20, height=16)
+ggsave("outputs/05_family_proportion/family_frequency_lengguru_site7-9.png", width=20, height=16)
 
 
 ## frequency of families in Mediterranean
@@ -132,23 +136,15 @@ ggsave("outputs/05_family_proportion/family_frequency_lengguru_site10-11.png", w
 
 ## Bellwood figures : proportion of families per site
 
-df_all_site <- rbind(count_families_site_caribbean[,c(-4)], count_families_site_lengguru[,c(-4)])
+df_all_site <- rbind(count_families_site_caribbean[,c(-6)], count_families_site_lengguru[,c(-6)])
 
-sites <- unique(df_all_site$site)
-count_sp_family_all_site=NULL
 
-for (i in 1:length(sites)) {
-  df <- df_all_site %>%
-    filter(site==sites[i])
-  df$n_fam <- sum(df$Freq)
-  count_sp_family_all_site <- rbind(count_sp_family_all_site, df) 
+family <- c("Serranidae", "Lutjanidae", "Acanthuridae", "Pomacentridae", "Balistidae", "Lethrinidae", "Scombridae", "Exocoetidae", "Myctophidae", "Apogonidae", "Carangidae", "Dasyatidae", "Haemulidae", "Clupeidae", "Gobiidae" )
+prop <- vector("list")
+for (i in 1:length(family)) {
+  fam <- df_all_site[df_all_site$family == family[i],]
+  prop[[i]] <- ggplot(fam, aes(n_motus_total, prop))+
+    geom_point()+
+    labs(title=family[i], x="total number of amplicons per site", y="Proportion")
+  ggsave(filename=paste("outputs/05_family_proportion/prop_",family[i],".png", sep = ""))
 }
-
-count_sp_family_all_site$perc <- count_sp_family_all_site$Freq/count_sp_family_all_site$n_fam 
-
-pomacentridae <- count_sp_family_all_site %>%
-  filter(Var1=="Pomacentridae")
-
-ggplot(pomacentridae, aes(n_fam, perc))+
-  geom_point()
-
