1f80b3bfdda7e079b7c8a64b1a8a68e0c8d6acaf
root <root@EB10PA4.newcefe.newage.fr>
2020-04-10 06:28:49 +0200

    afc stations

--- scripts/02_exploration/07_ACP_AFC_explo.R
+++ scripts/02_exploration/07_ACP_AFC_explo.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(reshape2)
 library(vegan)
 library(betapart)
+library(ade4)
 
 setwd("c:/Users/mathon/Desktop/linux/Global_eDNA/")
 load("Rdata/02_clean_all.Rdata")
@@ -25,9 +26,31 @@ rownames(pa_station) <- pa_station[,1]
 pa_station <- decostand(pa_station[,c(-1)], "pa",na.rm = TRUE)
 pa_station[is.na(pa_station)] <- 0
 pa_station <- as.data.frame(t(pa_station))
-
+pa_station$station <- rownames(pa_station)
 
 ## tableau metadata par station (region, site, prof, distance cote, latitude, methode, nb litre)
+metadata <- read.csv("metadata/Metadata_eDNA_global_V4.csv", stringsAsFactors = TRUE)
+metadata <- metadata %>%
+  filter(region%in%c("West_Papua", "French_Polynesia", "Caribbean", "East_Pacific"))
+metadata <- subset(metadata, !(station %in% c("estuaire_rio_don_diego_1", "estuaire_rio_don_diego_2", "estuaire_rio_don_diego_3")))
+metadata <- subset(metadata, !(sample_method %in% c("niskin", "control")))
+metadata <- subset(metadata, habitat=="marine")
+metadata <- metadata[,c("station", "region", "site", "latitude_start_clean", "volume", "sample_method", "depth", "dist_to_coast..m.")]
+metadata <- metadata %>%
+  distinct(station, .keep_all = TRUE)
+
+
+pa_station <- left_join(pa_station, metadata, by="station")
+
 ## faire ACP / AFC / dbRDA
 
-## plot richesse spe par station en foncton de latitude
+afc_pa_station <-dudi.coa(pa_station[,1:1603])
+pourc=round((afc_pa_station$eig/sum(afc_pa_station$eig))*100,2)
+pourc
+cumsum(pourc)
+
+s.label(afc_pa_station$co, clab=0.5, boxes=FALSE, sub="MOTUs AFC1") #representation des especes
+s.label(afc_pa_station$li, clab=0.5, sub="Stations") #representation des transects
+region<-as.factor(pa_station$region)
+s.class(afc_pa_station$li,region, col=c(1:4))
+
