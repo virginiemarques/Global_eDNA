58866127495951c4d2b2770f94e0394356c15d07
vmarques <virginie.marques@etu.umontpellier.fr>
2020-05-25 12:09:41 +0200

    correct accumulation curve RLS

--- scripts/02_exploration/01c_accumulation_curve_RLS.R
+++ scripts/02_exploration/01c_accumulation_curve_RLS.R
@@ -18,33 +18,65 @@ source('scripts/02_exploration/00_functions.R')
 RLS_families <- read.csv("data/RLS_families.csv", sep = ";")
 RLS_families <- RLS_families[,c(2,12:128)]
 
+# Functions 
+akaike.weights <- function(x)
+{
+  x <- x[!is.na(x)]
+  delta.aic <- x - min(x, na.rm = TRUE)
+  rel.LL <- exp(-0.5 * delta.aic)
+  sum.LL <- sum(rel.LL, na.rm = TRUE)
+  weights.aic <- rel.LL/sum.LL
+  return(list(deltaAIC = delta.aic, rel.LL = rel.LL, weights = weights.aic))
+}
 
 # ------------------------------------------------------------------------------- # 
 #### On family ----
 # ------------------------------------------------------------------------------- # 
 
-# rank_specify
-rank_choice = 'family'
-sample = "SiteCode"
+# method
+method_accumulation = "random"
 
-# Add a global saturation curve, i.e. all samples together?
-all_accumulation_RLS <- accumulation_curve_df(RLS_families, species_unit = rank_choice, column_station = sample) %>%
-  mutate(project_name = "All") %>%
-  select(project_name, richness, sd, sites)
+# Add a global saturation curve for RLS
+all_accumulation_RLS <- specaccum(RLS_families[,2:ncol(RLS_families)], 
+                      method = method_accumulation, permutations = 100,
+                      conditioned =TRUE, gamma = "jack1")
 
-save(all_accumulation_RLS, file = "Rdata/accumulation_families_RLS.rdata")
+# Accumulation for plot 
+all_accumulation_RLS_df <- data.frame(richness = all_accumulation_RLS$richness, sd = all_accumulation_RLS$sd, sites = all_accumulation_RLS$sites)
 
-# Asymptote of all plots 
-all_asymptote_RLS <- asymptote_mm(RLS_families, species_unit = rank_choice, column_station = sample) %>%
-  mutate(project_name = "All") %>%
-  select(project_name, asymptote)
+# Save
+save(all_accumulation_RLS_df, file = "Rdata/accumulation_families_RLS.rdata")
 
-save(all_asymptote_RLS, file = "Rdata/asymptote_families_RLS.rdata")
+# Asymptote of RLS  
 
-all_accumulation_RLS$asymptote <- all_asymptote_RLS$asymptote
+# models asymptotes
+lomo_edna <- fitspecaccum(all_accumulation_RLS, "lomolino")
+aic_lomo_edna <-AIC(lomo_edna)
+mm_edna <- fitspecaccum(all_accumulation_RLS, "michaelis-menten")
+aic_mm_edna <- AIC(mm_edna)
+gom_edna <- fitspecaccum(all_accumulation_RLS, "gompertz")
+aic_gom_edna <- AIC(gom_edna)
+asy_edna <- fitspecaccum(all_accumulation_RLS, "asymp")
+aic_asy_edna <- AIC(asy_edna)
+gis_edna <- fitspecaccum(all_accumulation_RLS, "logis")
+aic_gis_edna <- AIC(gis_edna)
+
+# compute results
+res_edna <- matrix(NA,nrow = 5, ncol = 3)
+rownames(res_edna) <- c("lomolino", "michaelis-menten", "gompertz", "asymp", "logis")
+colnames(res_edna) <- c("AIC", "Asymptote", "Weigth")
+res_edna[,"AIC"] <- c(aic_lomo_edna, aic_mm_edna, aic_gom_edna, aic_asy_edna, aic_gis_edna)
+res_edna[,"Weigth"] <- akaike.weights(c(aic_lomo_edna, aic_mm_edna, aic_gom_edna, aic_asy_edna, aic_gis_edna))$weights
+res_edna[,"Asymptote"] <- c(coef(lomo_edna)[[1]], coef(mm_edna)[[1]], coef(gom_edna)[[1]], coef(asy_edna)[[1]], coef(gis_edna)[[1]])
+
+# Calculation asymptote value
+asymp_RLS <- weighted.mean(res_edna[,"Asymptote"], res_edna[,"Weigth"])
+
+# Add to DF
+all_accumulation_RLS_df$asymptote <- asymp_RLS
 
 # plot
-family_RLS <- ggplot(all_accumulation_RLS) + 
+family_RLS <- ggplot(all_accumulation_RLS_df) + 
   geom_ribbon(aes(x = sites, ymin = richness-sd, ymax = richness+sd),  alpha = 0.5) +
   geom_line(aes(x = sites, y = richness)) +
   geom_hline(aes(yintercept = asymptote), linetype = "dashed", size = 1) +
