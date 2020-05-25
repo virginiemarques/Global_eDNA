a9a149a4c8500a8f5d708526d2c43845cc3afb62
root <root@EB10PA4.newcefe.newage.fr>
2020-05-25 08:13:18 +0200

    update plot figure1C

--- scripts/02_exploration/13_prediction_family_species_richness.R
+++ scripts/02_exploration/13_prediction_family_species_richness.R
@@ -17,6 +17,9 @@ asymptote_fam <- read.csv("outputs/10_acculation_curve_family/table_asymptote_fa
 
 # coral fish species data
 coral_fishes <- read.csv("data/Coral_fishes2.csv", sep=";")
+fam_coral  <- coral_fishes %>%
+  group_by(Family) %>%
+  summarise(n_species = n_distinct(Species))
 
 # our data
 df_all_filters <- df_all_filters %>%
@@ -24,12 +27,20 @@ df_all_filters <- df_all_filters %>%
   filter(station %ni% c("estuaire_rio_don_diego_1", "estuaire_rio_don_diego_2", "estuaire_rio_don_diego_3")) %>%
   filter(sample_method !="niskin" & region!="East_Pacific" & comment %ni% c("Distance decay 600m", "Distance decay 300m") & station!="glorieuse_distance_300m")
 
+fam_summary <- df_all_filters %>%
+  filter(!is.na(new_family_name)) %>%
+  group_by(new_family_name) %>%
+  summarise(n_motus = n_distinct(sequence))
+            
+fam_summary <- fam_summary%>%
+  filter(new_family_name%in%(fam_coral %>%filter(n_species > 5) %>%distinct(Family) %>% pull()))
 
-families10 <- as.character(asymptote_fam$family)
+families10 <- unique(fam_summary$new_family_name)
+
 
 # calculate number of species per family on the checklist
 for (i in 1:length(families10)) {
-  asymptote_fam[i,"n_species_checklist"] <- coral_fishes %>%
+  fam_summary[i,"n_species_checklist"] <- coral_fishes %>%
     subset(Family==families10[i]) %>%
     summarize(n_distinct(Species))
 }
@@ -37,7 +48,7 @@ for (i in 1:length(families10)) {
 # calculate number of taxa per family in our data
 
 for (i in 1:length(families10)) {
-  asymptote_fam[i,"n_species"] <- df_all_filters %>%
+  fam_summary[i,"n_taxa"] <- df_all_filters %>%
     subset(new_family_name==families10[i]) %>%
     filter(!is.na(new_scientific_name_ncbi)) %>%
     summarize(n_distinct(new_scientific_name_ncbi))
@@ -45,80 +56,80 @@ for (i in 1:length(families10)) {
 
 # calculate delta checklist - us (%)
 for (i in 1:length(families10)) {
-  asymptote_fam[i, "delta_taxa"] <- ((asymptote_fam[i, "n_species_checklist"] - asymptote_fam[i, "n_species"])*100)/asymptote_fam[i, "n_species_checklist"]
-  asymptote_fam[i, "delta_motu"] <- ((asymptote_fam[i, "n_species_checklist"] - asymptote_fam[i, "n_motus"])*100)/asymptote_fam[i, "n_species_checklist"]
-  asymptote_fam[i, "delta_asym"] <- ((asymptote_fam[i, "n_species_checklist"] - asymptote_fam[i, "n_asymtote"])*100)/asymptote_fam[i, "n_species_checklist"]
+  fam_summary[i, "delta_taxa"] <- ((fam_summary[i, "n_species_checklist"] - fam_summary[i, "n_taxa"])*100)/fam_summary[i, "n_species_checklist"]
+  fam_summary[i, "delta_motu"] <- ((fam_summary[i, "n_species_checklist"] - fam_summary[i, "n_motus"])*100)/fam_summary[i, "n_species_checklist"]
+  #fam_summary[i, "delta_asym"] <- ((fam_summary[i, "n_species_checklist"] - fam_summary[i, "n_asymtote"])*100)/fam_summary[i, "n_species_checklist"]
   
 }
 
 # calculate percentage us ~ checklist
 for (i in 1:length(families10)) {
-  asymptote_fam[i, "perc_taxa"] <- (asymptote_fam[i, "n_species"]*100)/asymptote_fam[i, "n_species_checklist"]
-  asymptote_fam[i, "perc_motu"] <- (asymptote_fam[i, "n_motus"]*100)/asymptote_fam[i, "n_species_checklist"]
-  asymptote_fam[i, "perc_asym"] <- (asymptote_fam[i, "n_asymtote"]*100)/asymptote_fam[i, "n_species_checklist"]
+  fam_summary[i, "perc_taxa"] <- (fam_summary[i, "n_taxa"]*100)/fam_summary[i, "n_species_checklist"]
+  fam_summary[i, "perc_motu"] <- (fam_summary[i, "n_motus"]*100)/fam_summary[i, "n_species_checklist"]
+  #fam_summary[i, "perc_asym"] <- (fam_summary[i, "n_asymtote"]*100)/fam_summary[i, "n_species_checklist"]
   
 }
 
 
 # tranform log richness
 for (i in 1:length(families10)) {
-  asymptote_fam[i, "log_taxa"] <- log(asymptote_fam[i, "n_species"])
-  asymptote_fam[i, "log_motu"] <- log(asymptote_fam[i, "n_motus"])
-  asymptote_fam[i, "log_asym"] <- log(asymptote_fam[i, "n_asymtote"])
-  asymptote_fam[i, "log_checklist"] <- log(asymptote_fam[i, "n_species_checklist"])
+  fam_summary[i, "log_taxa"] <- log10(fam_summary[i, "n_taxa"])
+  fam_summary[i, "log_motu"] <- log10(fam_summary[i, "n_motus"])
+  #fam_summary[i, "log_asym"] <- log10(fam_summary[i, "n_asymtote"])
+  fam_summary[i, "log_checklist"] <- log10(fam_summary[i, "n_species_checklist"])
   
 }
 
 # add short family name
 
-asymptote_fam$fam <- c("Acnt", "Apgn", "Blst", "Blnd", "Crng", "Chtd", "Dstd", "Gobd", "Hmld", "Hlcn", "Lbrd", "Lthr", "Ltjn", "Mnct", "Mlld", "Mrnd", "Pmcnth", "Pmcntr", "Scbr", "Srrn", "Sgnd", "Synd", "Ttrdt")
+#fam_summary$fam <- c("Acnt", "Apgn", "Blst", "Blnd", "Crng", "Chtd", "Dstd", "Gobd", "Hmld", "Hlcn", "Lbrd", "Lthr", "Ltjn", "Mnct", "Mlld", "Mrnd", "Pmcnth", "Pmcntr", "Scbr", "Srrn", "Sgnd", "Synd", "Ttrdt")
 
 
 
 # linear regression and plot for taxa
-lm_taxa <- lm(log_checklist~log_taxa, data=asymptote_fam)
+lm_taxa <- lm(log_checklist~log_taxa, data=fam_summary)
 summary(lm_taxa)
 
-plot_taxa <- ggplot(asymptote_fam, aes(log_taxa, log_checklist))+
+plot_taxa <- ggplot(fam_summary, aes(log_taxa, log_checklist))+
   geom_point(size=2)+
-  geom_text(aes(label=fam), size=3, position = position_jitter(width=0.5, height = 0.5))+
+  #geom_text(aes(label=fam), size=3, position = position_jitter(width=0.5, height = 0.5))+
   geom_abline(slope = 1, intercept = 0, color="red", size=0.8)+
-  geom_abline(slope = 0.5, intercept = 2.5, size=0.8)+
-  xlim(0,6)+
-  ylim(0,6)+
+  geom_abline(slope = 0.58, intercept = 1, size=0.8)+
+  xlim(0,2.5)+
+  ylim(0,2.5)+
   theme_bw()+
   labs(x="log(Number of taxa)",
        y="log(Number of species in the checklist)")
 
-grob <- grobTree(textGrob("y = 0.5x+2.5\nR² = 0.33\np < 0.005", x=0.8, y=0.2))
+grob <- grobTree(textGrob("y = 0.58x+1\nR² = 0.48\np < 0.001", x=0.8, y=0.2))
 plot_taxa2 <- plot_taxa+annotation_custom(grob)
 plot_taxa2
 
 
 # linear regression and plot for motus
-lm_motu <- lm(log_checklist~log_motu, data=asymptote_fam)
+lm_motu <- lm(log_checklist~log_motu, data=fam_summary)
 summary(lm_motu)
 
-plot_motu <- ggplot(asymptote_fam, aes(log_motu, log_checklist))+
+plot_motu <- ggplot(fam_summary, aes(log_motu, log_checklist))+
   geom_point(size=2)+
-  geom_text(aes(label=fam), size=3, position = position_jitter(width=0.5, height = 0.5))+
+  #geom_text(aes(label=fam), size=3, position = position_jitter(width=0.5, height = 0.5))+
   geom_abline(slope = 1, intercept = 0, color="red", size=0.8)+
-  geom_abline(slope = 0.7, intercept = 1.6, size=0.8)+
-  xlim(2,6)+
-  ylim(2,6)+
+  geom_abline(slope = 0.55, intercept = 0.86, size=0.8)+
+  xlim(0,2.5)+
+  ylim(0,2.5)+
   theme_bw()+
   labs(x="log(Number of MOTUs)",
        y="log(Number of species in the checklist)")
-grob <- grobTree(textGrob("y = 0.7x+1.6\nR² = 0.42\np < 0.001", x=0.8, y=0.2))
+grob <- grobTree(textGrob("y = 0.55x+0.86\nR² = 0.54\np < 0.001", x=0.8, y=0.2))
 plot_motu2 <- plot_motu+annotation_custom(grob)
 plot_motu2
 
 
 # linear regression and plot for asymptotes
-lm_asym <- lm(log_checklist~log_asym, data=asymptote_fam)
+lm_asym <- lm(log_checklist~log_asym, data=fam_summary)
 summary(lm_asym)
 
-plot_asym <- ggplot(asymptote_fam, aes(log_asym, log_checklist))+
+plot_asym <- ggplot(fam_summary, aes(log_asym, log_checklist))+
   geom_point(size=2)+
   geom_text(aes(label=fam), size=3, position = position_jitter(width=0.5, height = 0.5))+
   geom_abline(slope = 1, intercept = 0, color="red", size=0.8)+
@@ -134,10 +145,10 @@ plot_asym2
 
 
 # delta checklist - taxa
-lm_taxa <- lm(delta_taxa~n_species_checklist, data=asymptote_fam)
+lm_taxa <- lm(delta_taxa~n_species_checklist, data=fam_summary)
 summary(lm_taxa)
 
-delta_taxa <- ggplot(asymptote_fam, aes(n_species_checklist, delta_taxa))+
+delta_taxa <- ggplot(fam_summary, aes(n_species_checklist, delta_taxa))+
   geom_point(size=2)+
   geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
   ylim(0,110)+
@@ -149,10 +160,10 @@ delta_taxa2 <- delta_taxa+annotation_custom(grob)
 delta_taxa2
 
 # delta checklist - motu
-lm_motu <- lm(delta_motu~n_species_checklist, data=asymptote_fam)
+lm_motu <- lm(delta_motu~n_species_checklist, data=fam_summary)
 summary(lm_motu)
 
-delta_motu <- ggplot(asymptote_fam, aes(n_species_checklist, delta_motu))+
+delta_motu <- ggplot(fam_summary, aes(n_species_checklist, delta_motu))+
   geom_point(size=2)+
   geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
   ylim(-100,110)+
@@ -165,10 +176,10 @@ delta_motu2
 
 
 # delta checklist - asym
-lm_asym <- lm(delta_asym~n_species_checklist, data=asymptote_fam)
+lm_asym <- lm(delta_asym~n_species_checklist, data=fam_summary)
 summary(lm_asym)
 
-delta_asym <- ggplot(asymptote_fam, aes(n_species_checklist, delta_asym))+
+delta_asym <- ggplot(fam_summary, aes(n_species_checklist, delta_asym))+
   geom_point(size=2)+
   geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
   ylim(-150,110)+
@@ -181,40 +192,40 @@ delta_asym2
 
 
 # perc checklist - taxa
-lm_taxa <- lm(perc_taxa~n_species_checklist, data=asymptote_fam)
+lm_taxa <- lm(perc_taxa~n_species_checklist, data=fam_summary)
 summary(lm_taxa)
 
-perc_taxa <- ggplot(asymptote_fam, aes(n_species_checklist, perc_taxa))+
+perc_taxa <- ggplot(fam_summary, aes(n_species_checklist, perc_taxa))+
   geom_point(size=2)+
-  geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
+  #geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
   geom_hline(yintercept=100, color="red")+
   theme_bw()+
   labs(x="Number of species in the checklist",
        y="%(taxa ~ checklist)")
-grob <- grobTree(textGrob("R² = 0.04\np = 0.18", x=0.8, y=0.9))
+grob <- grobTree(textGrob("R² = 0.02\np = 0.41", x=0.8, y=0.9))
 perc_taxa2 <- perc_taxa+annotation_custom(grob)
 perc_taxa2
 
 # perc checklist - motu
-lm_motu <- lm(perc_motu~n_species_checklist, data=asymptote_fam)
+lm_motu <- lm(perc_motu~n_species_checklist, data=fam_summary)
 summary(lm_motu)
 
-perc_motu <- ggplot(asymptote_fam, aes(n_species_checklist, perc_motu))+
+perc_motu <- ggplot(fam_summary, aes(n_species_checklist, perc_motu))+
   geom_point(size=2)+
-  geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
+  #geom_text(aes(label=fam), size=3, position = position_jitter(width=10, height = 10))+
   geom_hline(yintercept=100, color="red")+
   theme_bw()+
   labs(x="Number of species in the checklist",
        y="%(MOTUs ~ checklist)")
-grob <- grobTree(textGrob("R² = 0.09\np = 0.08", x=0.8, y=0.9))
+grob <- grobTree(textGrob("R² = 0.006\np = 0.62", x=0.8, y=0.9))
 perc_motu2 <- perc_motu+annotation_custom(grob)
 perc_motu2
 
 # perc checklist - asym
-lm_asym <- lm(perc_asym~n_species_checklist, data=asymptote_fam)
+lm_asym <- lm(perc_asym~n_species_checklist, data=fam_summary)
 summary(lm_asym)
 
-perc_asym <- ggplot(asymptote_fam, aes(n_species_checklist, perc_asym))+
+perc_asym <- ggplot(fam_summary, aes(n_species_checklist, perc_asym))+
   geom_point(size=2)+
   geom_text(aes(label=fam), size=3, position = position_jitter(width=15, height = 15))+
   geom_hline(yintercept=100, color="red")+
@@ -229,7 +240,7 @@ perc_asym2
 
 
 # plot all together (les labels c'est degueulasse)
-plot_rich_perc <- ggarrange(plot_taxa2, plot_motu2, plot_asym2, perc_taxa2, perc_motu2, perc_asym2, nrow = 2, ncol = 3, labels = c("A", "B","C", "D", "E", "F"), label.x = -0.007, label.y = 1)
+plot_rich_perc <- ggarrange(plot_taxa2, plot_motu2, perc_taxa2, perc_motu2, nrow = 2, ncol = 2, labels = c("A", "B","C", "D"), label.x = -0.007, label.y = 1)
 plot_rich_perc
 
 
