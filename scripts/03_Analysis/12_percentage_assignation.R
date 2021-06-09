403c47181dab5ea7cccb176f475509a2c5cde40e
root <root@EB10PA4.newcefe.newage.fr>
2021-06-09 13:18:18 +0200

    script fig supp % assignation

---
+++ scripts/03_Analysis/12_percentage_assignation.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(reshape2)
+library(lisa)
+library(gridExtra)
+library(grid)
+library(cowplot)
+library(ggplot2)
+library(ggpubr)
+library(scales)
+
+
+load("Rdata/02-clean-data.Rdata")
+
+'%ni%' <- Negate("%in%")
+
+df_all_filters <- df_all_filters %>%
+  filter(province %in% c("Western_Indian_Ocean", "Southeast_Polynesia", "Tropical_Northwestern_Atlantic", "Western_Coral_Triangle", "Tropical_Southwestern_Pacific"))%>%
+  filter(station %ni% c("estuaire_rio_don_diego_1", "estuaire_rio_don_diego_2", "estuaire_rio_don_diego_3", "glorieuse_distance_300m")) %>%
+  filter(sample_method !="niskin" & comment %ni% c("Distance decay 600m", "Distance decay 300m"))%>%
+  filter(project != "Curacao") %>%
+  filter(habitat=="marine")%>%
+  filter(habitat_type %ni% c("BAIE"))%>%
+  filter(site35!="")
+
+df_all_filters <- df_all_filters %>%
+  filter(!is.na(family_name_corrected))
+
+perc_id <- df_all_filters %>% distinct(sequence, .keep_all=T)
+perc_id <- perc_id[,"best_identity_database"]
+
+perc_id2 <- as.data.frame(table(perc_id$best_identity_database))
+perc_id2$perc <- (perc_id2$Freq*100)/1500
+
+ggplot(perc_id2)+
+  geom_col(aes(x=Var1, y=perc))+
+  scale_x_discrete(breaks=c(0.85, 0.90, 0.95, 1))+
+  xlab("% of similarity with reference sequence")+
+  ylab("% of MOTUs")+
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        panel.background = element_blank())
+  
+
